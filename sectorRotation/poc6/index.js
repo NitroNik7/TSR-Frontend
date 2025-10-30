74299bdd6ea8c1e88169291adf5de2d20f6b6b43
nitronik7 <nikhil.harpale.edu@gmail.com>
2025-10-30 13:35:42 +0530

    minor updates

--- sectorRotation/poc6/index.js
+++ sectorRotation/poc6/index.js
@@ -580,14 +580,14 @@ function showSectorCard(option) {
                                     <h5>Stocks</h5>
                                 </div>
 
-                            <div class="mb-3">
-                                <button class="btn btn-outline-primary" onclick="updateSectorStocksTable('outPerforming')">
-                                    Out Performing
-                                </button>
-                                <button class="btn btn-outline-primary" onclick="updateSectorStocksTable('underPerforming')">
-                                    Under Performing
-                                </button>
-                            </div>
+
+                            <div class="mb-3 btn-group" role="group">
+  <input type="radio" class="btn-check" name="btnradio" id="outPerformingStocks" autocomplete="off" checked>
+  <label class="btn btn-outline-primary" for="outPerformingStocks"  onclick="updateSectorStocksTable('outPerforming')">Out Performing</label>
+
+  <input type="radio" class="btn-check" name="btnradio" id="underPerformingStocks" autocomplete="off">
+  <label class="btn btn-outline-primary" for="underPerformingStocks" onclick="updateSectorStocksTable('underPerforming')">Under Performing</label>
+</div>
 
                             <table id="${sectorStocksTableId}" class="table">
                              
