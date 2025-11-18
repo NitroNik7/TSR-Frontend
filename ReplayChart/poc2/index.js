eddb6fa66e6f28a854ac0d102ee4e57f5d85e110
nitronik7 <nikhil.harpale.edu@gmail.com>
2025-11-18 17:09:34 +0530

    updated replay chart toolbox ui

--- ReplayChart/poc2/index.js
+++ ReplayChart/poc2/index.js
@@ -7,7 +7,7 @@ var miChTb = (function () {
     var replayPeriodId = "tsrChReplayPeriod";
 
     var chartContainer = document.getElementById(chartContainerId);
-
+    var replayPeriodId = "tsrChReplayPeriod";
     var targetElement;
 
     function showReplayToolbar(e) {
@@ -30,13 +30,14 @@ var miChTb = (function () {
 
         let html = "";
 
-        html += `<div id=${toolbarDraggerId} class='tsr_chart_toolbar_box'>`;
-        html += `   <i class="fas fa-grip-vertical"></i>`;
-        html += `</div>`;
+        if (!isMobile()) {
+            html += `<div id=${toolbarDraggerId} class='tsr_chart_toolbar_box'>`;
+            html += `   <i class="fas fa-grip-vertical"></i>`;
+            html += `</div>`;
 
-        html += `<div style="border-left: 1px solid lightgray; margin-left: 3px; margin-right: 3px;">`;
-        html += `</div>`;
-
+            html += `<div style="border-left: 1px solid lightgray; margin-left: 3px; margin-right: 3px;">`;
+            html += `</div>`;
+        }
 
         html += getToolsContainer();
 
@@ -44,63 +45,74 @@ var miChTb = (function () {
 
         document.body.appendChild(toolbar);
 
-        let distanceFromTop = chartContainer.getBoundingClientRect().top  + window.scrollY;
-        let offset = 0.15 * window.innerHeight;
+        let distanceFromTop = chartContainer.getBoundingClientRect().top + window.scrollY;
+        // let offset = 0.15 * window.innerHeight;
 
-        toolbar.style.top = (distanceFromTop + offset) + "px";
+        let leftOffset = chartContainer.getBoundingClientRect().left + window.scrollX;
 
-        let dragger = document.getElementById(toolbarDraggerId);
-        dragger.onmousedown = mousedownHandler;
-
-        let x1, y1, x2, y2;
-
-        function mousedownHandler(e) {
-
-            e.preventDefault();
-
-            x1 = e.clientX;
-            y1 = e.clientY;
-
-            document.onmouseup = mouseupHandler;
-            document.body.onmousemove = mousemoveHandler;
+        if (isMobile()) {
+            toolbar.style.top = (distanceFromTop + 50) + "px";
+            toolbar.style.left = leftOffset + "px";
+        } else {
+            toolbar.style.top = (distanceFromTop + 50) + "px";
+            toolbar.style.left = "40vw";
         }
 
-        function mousemoveHandler(e) {
+        if (!isMobile()) {
 
-            e.preventDefault();
+            let dragger = document.getElementById(toolbarDraggerId);
+            dragger.onmousedown = mousedownHandler;
 
-            x2 = x1 - e.clientX;
-            y2 = y1 - e.clientY;
+            let x1, y1, x2, y2;
 
-            let boundingRect = chartContainer.getBoundingClientRect();
+            function mousedownHandler(e) {
 
-            dragger.style.cursor = "grabbing";
-            let leftBound = boundingRect.left + dragger.offsetWidth;
-            let rightBound = boundingRect.right - toolbar.offsetWidth;
+                e.preventDefault();
 
-            if (e.clientX > leftBound && e.clientX < rightBound) {
-                let offsetLeft = (toolbar.offsetLeft - x2) < 0 ? 0 : (toolbar.offsetLeft - x2);
-                toolbar.style.left = offsetLeft + 'px';
+                x1 = e.clientX;
+                y1 = e.clientY;
+
+                document.onmouseup = mouseupHandler;
+                document.body.onmousemove = mousemoveHandler;
             }
 
-            let topBound = boundingRect.top + toolbar.offsetHeight;
-            let bottomBound = boundingRect.bottom - toolbar.offsetHeight;
+            function mousemoveHandler(e) {
 
-            if (e.clientY > topBound && e.clientY < bottomBound) {
-                let offsetTop = (toolbar.offsetTop - y2) < 0 ? 0 : (toolbar.offsetTop - y2);
-                toolbar.style.top = offsetTop + 'px';
+                e.preventDefault();
+
+                x2 = x1 - e.clientX;
+                y2 = y1 - e.clientY;
+
+                let boundingRect = chartContainer.getBoundingClientRect();
+
+                dragger.style.cursor = "grabbing";
+                let leftBound = boundingRect.left + dragger.offsetWidth;
+                let rightBound = boundingRect.right - toolbar.offsetWidth;
+
+                if (e.clientX > leftBound && e.clientX < rightBound) {
+                    let offsetLeft = (toolbar.offsetLeft - x2) < 0 ? 0 : (toolbar.offsetLeft - x2);
+                    toolbar.style.left = offsetLeft + 'px';
+                }
+
+                let topBound = boundingRect.top + toolbar.offsetHeight;
+                let bottomBound = boundingRect.bottom - toolbar.offsetHeight;
+
+                if (e.clientY > topBound && e.clientY < bottomBound) {
+                    let offsetTop = (toolbar.offsetTop - y2) < 0 ? 0 : (toolbar.offsetTop - y2);
+                    toolbar.style.top = offsetTop + 'px';
+                }
+
+                x1 = e.clientX;
+                y1 = e.clientY;
             }
 
-            x1 = e.clientX;
-            y1 = e.clientY;
-        }
+            function mouseupHandler(e) {
+                dragger.style.cursor = "grab";
 
-        function mouseupHandler(e) {
-            dragger.style.cursor = "grab";
-
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.body.onmousemove = null;
+                // stop moving when mouse button is released:
+                document.onmouseup = null;
+                document.body.onmousemove = null;
+            }
         }
 
         // document.onmousedown = function (e) {
@@ -115,90 +127,89 @@ var miChTb = (function () {
         let html = "";
 
         html += `<div class="d-flex align-items-center">`
-        html += `<div class="text-center">   `
-        html += `<span style="font-size: 12px;">Select Period</span>`
-        html += `<div class="dropdown-center mt-1">`
-        html += `   <button id="tsrChReplayPeriod" class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">`
-        html += `       From Chart`
-        html += `   </button>`
-        html += `   <ul class="dropdown-menu">`
-        html += `       <li>`
-        html += `           <button class="btn btn-sm" onclick="miChTb.urp('chart')">From Chart</button>`
-        html += `       </li>`
-        html += `       <li>`
-        html += `           <div class="dropend">`
-        html += `               <button type="button" class="btn btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside" data-bs-offset="-41,28">`
-        html += `                   Select Start Date`
-        html += `               </button>`
-        html += `               <form class="dropdown-menu p-2 text-end">`
-        html += `                   <div class="input-group input-group-sm form-floating mb-2">`
-        html += `                       <input type="datetime-local" class="form-control" id="tsrReplayStartDate" required>`
-        html += `                       <label for="tsrReplayStartDate" class="form-label">Select Date & Time</label>`
+        html += `   <div class="text-center">   `
+        html += `       <span style="font-size: 12px;">Select Period</span>`
+        html += `       <div class="dropdown-center mt-1">`
+        html += `          <button id="${replayPeriodId}" class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">`
+        html += `               Select Candle`
+        html += `           </button>`
+        html += `           <ul class="dropdown-menu">`
+        html += `               <li>`
+        html += `                   <button class="btn btn-sm" onclick="miChTb.urp('chart')">Select Candle</button>`
+        html += `               </li>`
+        html += `               <li>`
+        html += `                   <div class="dropend">`
+        html += `                       <button type="button" class="btn btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside" data-bs-offset="-41,28">`
+        html += `                           Select Start Date`
+        html += `                       </button>`
+        html += `                       <form class="dropdown-menu p-2 text-end">`
+        html += `                           <div class="input-group input-group-sm form-floating mb-2">`
+        html += `                               <input type="datetime-local" class="form-control" id="tsrReplayStartDate" required>`
+        html += `                               <label for="tsrReplayStartDate" class="form-label">Select Date & Time</label>`
+        html += `                           </div>`
+        html += `                           <button type="button" class="btn btn-secondary btn-sm" onclick="miChTb.urp('startDate')">Next</button>`
+        html += `                       </form>`
         html += `                   </div>`
-        html += `                   <button type="button" class="btn btn-secondary btn-sm" onclick="miChTb.urp('startDate')">Next</button>`
-        html += `               </form>`
-        html += `           </div>`
-        html += `       </li>`
-        html += `       <li>`
-        html += `           <div class="dropend">`
-        html += `               <button type="button" class="btn dropdown-toggle btn-sm" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside" data-bs-offset="-70,60">`
-        html += `                   Select Ticks`
-        html += `               </button>`
-        html += `               <form class="dropdown-menu p-2 text-end">`
-        html += `                   <div class="input-group input-group-sm form-floating mb-2">`
-        html += `                       <input type="number" class="form-control" id="tsrReplayTicks" size="4" value="10" required>`
-        html += `                       <label for="tsrReplayTicks" class="form-label">Enter Ticks</label>`
+        html += `               </li>`
+        html += `               <li>`
+        html += `                   <div class="dropend">`
+        html += `                       <button type="button" class="btn dropdown-toggle btn-sm" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside" data-bs-offset="-70,60">`
+        html += `                           Select Ticks`
+        html += `                       </button>`
+        html += `                       <form class="dropdown-menu p-2 text-end">`
+        html += `                           <div class="input-group input-group-sm form-floating mb-2">`
+        html += `                               <input type="number" class="form-control" id="tsrReplayTicks" size="4" value="10" required>`
+        html += `                               <label for="tsrReplayTicks" class="form-label">Enter Ticks</label>`
+        html += `                           </div>`
+        html += `                           <button type="button" class="btn btn-secondary btn-sm" style="margin: 0;"  onclick="miChTb.urp('tick')">Next</button>`
+        html += `                       </form>`
         html += `                   </div>`
-        html += `                   <button type="button" class="btn btn-secondary btn-sm" style="margin: 0;"  onclick="miChTb.urp('tick')">Next</button>`
-        html += `               </form>`
-        html += `           </div>`
-        html += `       </li>`
-        html += `   </ul>`
-        html += `</div>`;
-        html += `</div>`;
+        html += `               </li>`
+        html += `           </ul>`
+        html += `       </div>`;
+        html += `   </div>`;
 
-        html += `<div class="vr"></div> `;
+        html += `   <div class="vr"></div> `;
 
-        html += `<div class="text-center mx-2">`
-        html += `   <span style="font-size: 12px;">Auto run</span>`
-        html += `   <div class="d-flex mt-1">`
-        html += `       <button id="tsrReplayPlayPauseBtn" class="btn" style="margin: 0;" title="Play">`
-        html += `           <i class="fas fa-play"></i>`
-        html += `       </button>`
-        html += `       <select id="tsrReplaySpeedSelect" class="form-select form-select-sm" aria-label="Small select example" title="Select Speed">`
-        html += `           <option value="0.5">0.5x</option>`
-        html += `           <option value="0.75">0.75x</option>`
-        html += `           <option selected>1x</option>`
-        html += `           <option value="2">2x</option>`
-        html += `           <option value="5">5x</option>`
-        html += `       </select>`
+        html += `   <div class="text-center mx-2">`
+        html += `       <span style="font-size: 12px;">Auto run</span>`
+        html += `       <div class="d-flex mt-1">`
+        html += `           <button id="tsrReplayPlayPauseBtn" class="btn" style="margin: 0;" title="Play">`
+        html += `               <i class="fas fa-play"></i>`
+        html += `           </button>`
+        html += `           <select id="tsrReplaySpeedSelect" class="form-select form-select-sm" aria-label="Small select example" title="Select Speed">`
+        html += `               <option value="0.5">0.5x</option>`
+        html += `               <option value="0.75">0.75x</option>`
+        html += `               <option selected>1x</option>`
+        html += `               <option value="2">2x</option>`
+        html += `               <option value="5">5x</option>`
+        html += `           </select>`
+        html += `       </div>`
         html += `   </div>`
 
-        html += `</div>`
+        html += `   <div class="vr"></div>`
 
-        html += `<div class="vr"></div>`
-
-        html += `<div class="text-center mx-2">`
-        html += `   <span style="font-size: 12px;">Manual run</span>`
-        html += `   <div class="d-flex mt-1">`
-        html += `       <button id="tsrReplayStepBackwardBtn" class="btn" style="margin: 0;"  title="Step Backward">`
-        html += `           <i class="fas fa-step-backward"></i>`
-        html += `       </button>`
-        html += `       <button id="tsrReplayStepForwardBtn" class="btn" style="margin: 0;"  title="Step Forward">`
-        html += `           <i class="fas fa-step-forward"></i>`
-        html += `       </button>`
+        html += `   <div class="text-center mx-2">`
+        html += `      <span style="font-size: 12px;">Manual run</span>`
+        html += `      <div class="d-flex mt-1">`
+        html += `          <button id="tsrReplayStepBackwardBtn" class="btn" style="margin: 0;"  title="Step Backward">`
+        html += `              <i class="fas fa-step-backward"></i>`
+        html += `          </button>`
+        html += `          <button id="tsrReplayStepForwardBtn" class="btn" style="margin: 0;"  title="Step Forward">`
+        html += `              <i class="fas fa-step-forward"></i>`
+        html += `          </button>`
+        html += `      </div>`
         html += `   </div>`
-        html += `</div>`
 
-        html += `<div class="vr"></div>`
+        html += `   <div class="vr"></div>`
 
-        html += `<div>`
-        html += `   <button id="tsrReplayRestartBtn" class="btn" style="margin: 0;"  title="Restart">`
-        html += `       <i class="fas fa-redo"></i>`
-        html += `   </button>`
-        html += `   <button class="btn btn-close" style="margin: 0;"  onclick="miChTb.de()" title="Close">`
-        html += `   </button>`
-        html += `</div>`;
+        html += `   <div class="d-flex">`
+        html += `      <button id="tsrReplayRestartBtn" class="btn" style="margin: 0;"  title="Restart">`
+        html += `          <i class="fas fa-redo"></i>`
+        html += `      </button>`
+        html += `      <button class="btn btn-close" style="margin: 0;"  onclick="miChTb.de()" title="Close">`
+        html += `      </button>`
+        html += `   </div>`;
 
         html += `</div>`;
 
@@ -226,9 +237,20 @@ var miChTb = (function () {
             let input = document.getElementById("tsrReplayStartDate");
             html += "Start Date <br>" + input.value;
         } else {
-            html += "From Chart";
+            html += "Select Candle";
         }
         button.innerHTML = html;
+
+        let replayPeriodDropdown = document.getElementById(replayPeriodId);
+        bootstrap.Dropdown.getInstance(replayPeriodDropdown).hide();
+    }
+
+    function isMobile() {
+        if (window.innerWidth < 768) {
+            return true;
+        }
+
+        return false;
     }
 
     return {
